37275802a62c559066554b6c56b30102cdb5cf03
Brendan O'Connor <brenocon@gmail.com>
2015-01-30 17:32:56 -0500

    fix build.sh which broke with the package dir change

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/bin/zsh
 set -eux
+cd $(dirname $0)/stanford_corenlp_pywrapper
+
 rm -rf _build lib/piperunner.jar
 mkdir _build
 
-javac -source 7 -target 7 -d _build -cp "$(print lib/*.jar | tr ' ' ':')":../stanford-corenlp-3.3.1.jar javasrc/**/*.java
+CORENLP_JAR=$HOME/stanford-corenlp/stanford-corenlp-3.4.jar
+
+javac -source 7 -target 7 -d _build -cp "$(print lib/*.jar | tr ' ' ':')":$CORENLP_JAR javasrc/**/*.java
 (cd _build && jar cf ../lib/piperunner.jar .)
 ls -l lib/piperunner.jar
+
+rm -rf _build
