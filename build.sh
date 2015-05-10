df5ff4666456f23ad7b6f5a7d3e95a24bc294aa3
Brendan O'Connor <brenocon@gmail.com>
2015-05-10 12:58:39 -0400

    update build.sh corenlp version too

--- build.sh
+++ build.sh
@@ -5,8 +5,7 @@ cd $(dirname $0)/stanford_corenlp_pywrapper
 rm -rf _build lib/piperunner.jar
 mkdir _build
 
-# CORENLP_JAR=$HOME/stanford-corenlp/stanford-corenlp-3.5.1.jar
-CORENLP_JAR=/home/sw/stanford-corenlp-full-2015-01-30/stanford-corenlp-3.5.1.jar
+CORENLP_JAR=/home/sw/corenlp/stanford-corenlp-full-2015-04-20/stanford-corenlp-3.5.2.jar
 
 javac -source 7 -target 7 -d _build -cp "$(print lib/*.jar | tr ' ' ':')":$CORENLP_JAR javasrc/**/*.java
 (cd _build && jar cf ../lib/piperunner.jar .)
